5ce5988473286defcb861011a573899066a82402
Annargb <130927958+Annargb@users.noreply.github.com>
2024-04-02 07:59:46 +0000

    Deploying to gh-pages from @ Annargb/advertCamper@15677ad0fd175f3e4725443d97dc57ce0640aa7b 🚀

---
+++ assets/FavoritePage-078cd92f.js
@@ -0,0 +1,15 @@
+import{u as s,a as r,b as i,r as n,m as c,j as e}from"./index-c04d98f8.js";import{f as l,N as m,C as p}from"./NotFound-84f0418f.js";import"./van1-33f09857.js";const d=s.div`
+  padding-bottom: 100px;
+`,x=s.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`,u=s.h1`
+  margin-top: 28px;
+  margin-bottom: 44px;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 1.5;
+  color: ${o=>o.theme.colors.textTitleColor};
+`,g=()=>{const o=r(l),a=i();return n.useEffect(()=>{const t=JSON.parse(localStorage.getItem("favorites"));t!=null&&a(c(t))},[a]),e.jsxs(d,{children:[e.jsx(u,{children:"Favorite"}),e.jsx(x,{children:o.length?e.jsx("ul",{children:o.length!==0&&o.map(t=>e.jsx(p,{camper:t},t.id))}):e.jsx(m,{text:"It seems like you don't have your favorite campervans"})})]})};export{g as default};
